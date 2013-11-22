d7984f5764d3165f8cc5e83c8696bc515dd931b4
Dan Vrátil <dvratil@redhat.com>
2013-11-22 11:04:07 +0100

    Fix installation of the Plasma applet

---
+++ plasma/package/metadata.desktop.cmake
@@ -0,0 +1,14 @@
+[Desktop Entry]
+Type=Service
+X-Plasma-API=declarativeappletscript
+X-Plasma-MainScript=ui/main.qml
+ 
+X-KDE-PluginInfo-Author=Dan Vrátil
+X-KDE-PluginInfo-Email=dvratil@redhat.com
+X-KDE-PluginInfo-Name=org.kde.plasma.kscreen.qml
+X-KDE-PluginInfo-Version=@KSCREEN_VERSION@
+X-KDE-PluginInfo-Website=http://www.kde.org
+X-KDE-PluginInfo-Category=System Information
+X-KDE-PluginInfo-Depends=
+X-KDE-PluginInfo-License=GPL
+X-KDE-PluginInfo-EnabledByDefault=true
