b86bbc8895ce6ea8fe2ffb3983460283b7d51dff
Sebastian Kügler <sebas@kde.org>
2016-10-11 05:20:00 +0200

    add small testapp for osd classes

---
+++ tests/osd/osdtest.h
@@ -0,0 +1,42 @@
+/*************************************************************************************
+ *  Copyright 2016 Sebastian Kügler <sebas@kde.org>                                  *
+ *                                                                                   *
+ *  This library is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU Lesser General Public                       *
+ *  License as published by the Free Software Foundation; either                     *
+ *  version 2.1 of the License, or (at your option) any later version.               *
+ *                                                                                   *
+ *  This library is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU                *
+ *  Lesser General Public License for more details.                                  *
+ *                                                                                   *
+ *  You should have received a copy of the GNU Lesser General Public                 *
+ *  License along with this library; if not, write to the Free Software              *
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA       *
+ *************************************************************************************/
+
+#ifndef KSCREEN_OSDTEST_H
+#define KSCREEN_OSDTEST_H
+
+#include <QObject>
+
+
+namespace KScreen
+{
+
+class OsdTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit OsdTest(QObject *parent = nullptr);
+    virtual ~OsdTest();
+
+    void start();
+
+};
+
+} // namespace
+
+#endif // KSCREEN_OSDTEST_H
