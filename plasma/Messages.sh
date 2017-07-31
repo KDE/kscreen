57b4ff35f25d3fb30b67f7264c177f4f87e1e9b6
Sebastian Kügler <sebas@kde.org>
2017-07-31 11:25:18 +0200

    Delete old plasmoid, it's useless in today's world

--- plasma/Messages.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-$XGETTEXT `find . -name '*.cpp'` -o $podir/plasma_applet_org.kde.plasma.kscreen.pot
-$XGETTEXT `find . -name '*.qml'` -j -o $podir/plasma_applet_org.kde.plasma.kscreen.pot
-rm -f rc.cpp
