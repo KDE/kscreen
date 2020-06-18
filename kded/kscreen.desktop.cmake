7b6208785cc8fa7f900487e8cfa8001493c98d71
l10n daemon script <scripty@kde.org>
2020-06-18 09:05:22 +0200

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- kded/kscreen.desktop.cmake
+++ kded/kscreen.desktop.cmake
@@ -43,6 +43,7 @@ Name[sv]=Kscreen 2
 Name[tg]=KScreen 2
 Name[tr]=KScreen 2
 Name[uk]=KScreen 2
+Name[x-test]=xxKScreen 2xx
 Name[zh_CN]=KScreen 2
 Name[zh_TW]=KScreen 2
 Comment=Screen management
@@ -89,6 +90,7 @@ Comment[sv]=Skärmhantering
 Comment[tg]=Идоракунии экран
 Comment[tr]=Ekran yönetimi
 Comment[uk]=Керування екраном
+Comment[x-test]=xxScreen managementxx
 Comment[zh_CN]=屏幕管理
 Comment[zh_TW]=螢幕管理
 
