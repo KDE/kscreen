KDE/kscreen

commit 49c5d9c5db553a745534099484a6f3e7c6b7bcf4

refactor(kded): refactor OSD

kded/qml/OsdItem.qml
@@ -18,39 +18,26 @@
 
 import QtQuick 2.5
 import QtQuick.Window 2.2
+import QtQuick.Layouts 1.10
 
 import org.kde.plasma.core 2.0 as PlasmaCore
 import org.kde.plasma.extras 2.0 as PlasmaExtras
 
-Item {
-    property QtObject rootItem
-
-    height: Math.min(units.gridUnit * 15, Screen.desktopAvailableHeight / 5)
-    width: height
-
+ColumnLayout {
     PlasmaCore.IconItem {
-        id: icon
-        height: parent.height - label.height - ((units.smallSpacing/2) * 3)
-        width: parent.width
         source: rootItem.icon
+
+        Layout.preferredHeight: PlasmaCore.Units.gridUnit * 10
+        Layout.preferredWidth: implicitHeight
     }
 
     PlasmaExtras.Heading {
-        id: label
-        anchors {
-            bottom: parent.bottom
-            left: parent.left
-            right: parent.right
-            margins: Math.floor(units.smallSpacing / 2)
-        }
-
         text: rootItem.infoText
+
         horizontalAlignment: Text.AlignHCenter
         wrapMode: Text.WordWrap
         maximumLineCount: 2
-        elide: Text.ElideLeft
-        minimumPointSize: theme.defaultFont.pointSize
-        fontSizeMode: Text.HorizontalFit
+
+        Layout.fillWidth: true
     }
-    Component.onCompleted: print("osditem loaded..." + root.infoText);
 }

kded/qml/OutputIdentifier.qml
@@ -20,7 +20,7 @@ import QtQuick.Layouts 1.3
 import QtQuick.Window 2.2
 
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
+import org.kde.plasma.components 3.0 as PlasmaComponents
 
 ColumnLayout {
 

kded/qml/OsdSelector.qml
@@ -17,10 +17,11 @@
 
 import QtQuick 2.5
 import QtQuick.Window 2.2
+import QtQuick.Layouts 1.10
 
 import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 2.0 as PlasmaComponents
 import org.kde.plasma.extras 2.0 as PlasmaExtras
+import org.kde.plasma.components 3.0 as PlasmaComponents
 
 import org.kde.KScreen 1.0
 
@@ -38,17 +39,8 @@ PlasmaCore.Dialog {
 
     signal clicked(int actionId)
 
-    mainItem: Item {
-        height: Math.min(units.gridUnit * 15, Screen.desktopAvailableHeight / 5)
-        width: buttonRow.width
-
-        Row {
-            id: buttonRow
-            spacing: theme.defaultFont.pointSize
-
-            height: parent.height - label.height - ((units.smallSpacing/2) * 3)
-            width: (actionRepeater.count * height) + ((actionRepeater.count - 1) * buttonRow.spacing);
-
+    mainItem: ColumnLayout {
+        RowLayout {
             Repeater {
                 id: actionRepeater
                 property int currentIndex: 0
@@ -59,25 +51,21 @@ PlasmaCore.Dialog {
                             label: OsdAction.actionLabel(layout),
                             action: layout
                         }
-                    });
+                    })
                 }
                 delegate: PlasmaComponents.Button {
-                    property var action: modelData.action
+                    action: modelData.action
                     Accessible.name: modelData.label
-                    PlasmaCore.IconItem {
-                        source: modelData.iconSource
-                        height: buttonRow.height - ((units.smallSpacing / 2) * 3)
-                        width: height
-                        anchors.centerIn: parent
-                    }
-                    height: parent.height
-                    width: height
 
+                    icon.name: modelData.iconSource
+                    icon.height: PlasmaCore.Units.gridUnit * 8
+                    icon.width: PlasmaCore.Units.gridUnit * 8
+
+                    onClicked: root.clicked(action)
                     onHoveredChanged: {
                         actionRepeater.currentIndex = index
                     }
 
-                    onClicked: root.clicked(action)
                     activeFocusOnTab: true
 
                     // use checked only indirectly, since its binding will break
@@ -99,62 +87,35 @@ PlasmaCore.Dialog {
                 }
             }
         }
-
         PlasmaExtras.Heading {
-            id: label
-            anchors {
-                bottom: parent.bottom
-                left: parent.left
-                right: parent.right
-                margins: Math.floor(units.smallSpacing / 2)
-            }
-
             text: root.infoText
             horizontalAlignment: Text.AlignHCenter
-            wrapMode: Text.WordWrap
             maximumLineCount: 2
-            elide: Text.ElideLeft
-            minimumPointSize: theme.defaultFont.pointSize
-            fontSizeMode: Text.HorizontalFit
+            wrapMode: Text.WordWrap
+
+            Layout.fillWidth: true
+            Layout.margins: Math.floor(PlasmaCore.Units.smallSpacing / 2)
         }
 
-        Component.onCompleted: print("OsdSelector loaded...");
-
-        function next() {
-            var index = actionRepeater.currentIndex + 1
-            if (index >= actionRepeater.count) {
-                index = 0
-            }
-            actionRepeater.currentIndex = index
-        }
-        function previous() {
-            var index = actionRepeater.currentIndex - 1
-            if (index < 0) {
-                index = actionRepeater.count - 1
-            }
-            actionRepeater.currentIndex = index
+        function move(delta) {
+            actionRepeater.currentIndex = ((actionRepeater.currentIndex + delta) + actionRepeater.count) % actionRepeater.count
         }
 
         Keys.onPressed: {
-            if (event.key === Qt.Key_Return || event.key === Qt.Key_Enter) {
-                event.accepted = true
-                clicked(buttonRow.children[actionRepeater.currentIndex].action)
-                return
-            }
-            if (event.key === Qt.Key_Right) {
-                event.accepted = true
-                next()
-                return
-            }
-            if (event.key === Qt.Key_Left) {
-                event.accepted = true
-                previous()
-                return
-            }
-            if (event.key === Qt.Key_Escape) {
-                event.accepted = true
-                clicked(OsdAction.NoAction)
-                return
+            switch (event.key) {
+                case Qt.Key_Return:
+                case Qt.Key_Enter:
+                    clicked(actionRepeater.itemAt(actionRepeater.currentIndex).action)
+                    break
+                case Qt.Key_Right:
+                    move(1)
+                    break
+                case Qt.Key_Left:
+                    move(-1)
+                    break
+                case Qt.Key_Escape:
+                    clicked(OsdAction.NoAction)
+                    break
             }
         }
     }

commit b499deca0b078daf1276dc141cb981df94ed3ce3

[KDED] Remove showOutputIdentifiers and showOsd and dbus methods
AKA org.kde.kscreen.osdService.showOutputIdentifiers and org.kde.kscreen.osdService.showOsd

Keeps org.kde.kscreen.osdService.showActionSelector

kscreen has an osdmanager, where its osd are broken since
https://invent.kde.org/plasma/kscreen/-/commit/49c5d9c5db553a745534099484a6f3e7c6b7bcf4 
except for the only important used one: the "select screen action" on screen connection.

The other two--screen and osdText--are unused; see
https://lxr.kde.org/search?%21v=kf5-qt5&_filestring=&_string=kscreen.osdService

The showText usecase is covered by plasma-workspace osd and identify output is implemented in the KCM.

So remove those to limit maintenance burden.

kded/CMakeLists.txt
@@ -48,10 +48,7 @@ kcoreaddons_desktop_to_json(kscreen ${CMAKE_CURRENT_BINARY_DIR}/kscreen.desktop)
 install(TARGETS kscreen DESTINATION ${KDE_INSTALL_PLUGINDIR}/kf5/kded)
 
 set(QML_FILES
-    qml/Osd.qml
-    qml/OsdItem.qml
     qml/OsdSelector.qml
-    qml/OutputIdentifier.qml
 )
 
 install(FILES ${QML_FILES} DESTINATION ${KDE_INSTALL_DATADIR}/kded_kscreen/qml)

kded/daemon.cpp
@@ -339,21 +339,6 @@ void KScreenDaemon::saveCurrentConfig()
     }
 }
 
-void KScreenDaemon::showOsd(const QString &icon, const QString &text)
-{
-    QDBusMessage msg = QDBusMessage::createMethodCall(QLatin1String("org.kde.plasmashell"),
-                                                      QLatin1String("/org/kde/osdService"),
-                                                      QLatin1String("org.kde.osdService"),
-                                                      QLatin1String("showText"));
-    msg << icon << text;
-    QDBusConnection::sessionBus().asyncCall(msg);
-}
-
-void KScreenDaemon::showOutputIdentifier()
-{
-    m_osdManager->showOutputIdentifiers();
-}
-
 void KScreenDaemon::displayButton()
 {
     qCDebug(KSCREEN_KDED) << "displayBtn triggered";

kded/daemon.h
@@ -63,7 +63,6 @@ private:
     void setMonitorForChanges(bool enabled);
 
     void outputConnectedChanged();
-    void showOutputIdentifier();
     void applyOsdAction(KScreen::OsdAction::Action action);
 
     void doApplyConfig(const KScreen::ConfigPtr &config);
@@ -72,7 +71,6 @@ private:
 
     void monitorConnectedChange();
     void disableOutput(const KScreen::OutputPtr &output);
-    void showOsd(const QString &icon, const QString &text);
 
     void updateOrientation();
 

kded/osd.cpp
@@ -29,76 +29,11 @@ Osd::Osd(const KScreen::OutputPtr &output, QObject *parent)
 {
     connect(output.data(), &KScreen::Output::isConnectedChanged, this, &Osd::onOutputAvailabilityChanged);
     connect(output.data(), &KScreen::Output::isEnabledChanged, this, &Osd::onOutputAvailabilityChanged);
-    connect(output.data(), &KScreen::Output::currentModeIdChanged, this, &Osd::updatePosition);
     connect(output.data(), &KScreen::Output::destroyed, this, &Osd::hideOsd);
 }
 
 Osd::~Osd()
 {
-}
-
-bool Osd::initOsd()
-{
-    if (m_osdObject) {
-        return true;
-    }
-
-    const QString osdPath = QStandardPaths::locate(QStandardPaths::QStandardPaths::GenericDataLocation, QStringLiteral("kded_kscreen/qml/Osd.qml"));
-    if (osdPath.isEmpty()) {
-        qCWarning(KSCREEN_KDED) << "Failed to find OSD QML file" << osdPath;
-        return false;
-    }
-
-    m_osdObject = new KDeclarative::QmlObjectSharedEngine(this);
-    m_osdObject->setSource(QUrl::fromLocalFile(osdPath));
-
-    if (m_osdObject->status() != QQmlComponent::Ready) {
-        qCWarning(KSCREEN_KDED) << "Failed to load OSD QML file" << osdPath;
-        delete m_osdObject;
-        m_osdObject = nullptr;
-        return false;
-    }
-
-    m_timeout = m_osdObject->rootObject()->property("timeout").toInt();
-    m_osdTimer = new QTimer(this);
-    m_osdTimer->setSingleShot(true);
-    connect(m_osdTimer, &QTimer::timeout, this, &Osd::hideOsd);
-    return true;
-}
-
-void Osd::showGenericOsd(const QString &icon, const QString &text)
-{
-    if (!initOsd()) {
-        return;
-    }
-
-    m_outputGeometry = m_output->geometry();
-    auto *rootObject = m_osdObject->rootObject();
-    rootObject->setProperty("itemSource", QStringLiteral("OsdItem.qml"));
-    rootObject->setProperty("infoText", text);
-    rootObject->setProperty("icon", icon);
-
-    showOsd();
-}
-
-void Osd::showOutputIdentifier(const KScreen::OutputPtr &output, bool shouldShouldSerialNumber)
-{
-    if (!initOsd()) {
-        return;
-    }
-
-    m_outputGeometry = output->geometry();
-
-    auto *rootObject = m_osdObject->rootObject();
-    auto mode = output->currentMode();
-    QSize realSize = mode->size();
-    if (!output->isHorizontal()) {
-        realSize.transpose();
-    }
-    rootObject->setProperty("itemSource", QStringLiteral("OutputIdentifier.qml"));
-    rootObject->setProperty("modeName", Utils::sizeToString(realSize));
-    rootObject->setProperty("outputName", Utils::outputName(output, shouldShouldSerialNumber));
-    showOsd();
 }
 
 void Osd::showActionSelector()
@@ -146,62 +81,10 @@ void Osd::onOutputAvailabilityChanged()
     }
 }
 
-void Osd::updatePosition()
-{
-    if (!initOsd()) {
-        return;
-    }
-
-    const auto geometry = m_output->geometry();
-    if (!geometry.isValid()) {
-        hideOsd();
-    }
-
-    auto *rootObject = m_osdObject->rootObject();
-
-    const int dialogWidth = rootObject->property("width").toInt();
-    const int dialogHeight = rootObject->property("height").toInt();
-    const int relx = geometry.x();
-    const int rely = geometry.y();
-    const int pos_x = relx + (geometry.width() - dialogWidth) / 2;
-    const int pos_y = rely + (geometry.height() - dialogHeight) / 2;
-
-    rootObject->setProperty("x", pos_x);
-    rootObject->setProperty("y", pos_y);
-}
-
-void Osd::showOsd()
-{
-    m_osdTimer->stop();
-
-    auto *rootObject = m_osdObject->rootObject();
-
-    // only animate on X11, wayland plugin doesn't support this and
-    // pukes loads of warnings into our logs
-    if (qGuiApp->platformName() == QLatin1String("xcb")) {
-        if (rootObject->property("timeout").toInt() > 0) {
-            rootObject->setProperty("animateOpacity", false);
-            rootObject->setProperty("opacity", 1);
-            rootObject->setProperty("animateOpacity", true);
-            rootObject->setProperty("opacity", 0);
-        }
-    }
-    rootObject->setProperty("visible", true);
-    QTimer::singleShot(0, this, &Osd::updatePosition);
-    if (m_timeout > 0) {
-        m_osdTimer->start(m_timeout);
-    }
-}
-
 void Osd::hideOsd()
 {
     if (m_osdActionSelector) {
         if (auto *rootObject = m_osdActionSelector->rootObject()) {
-            rootObject->setProperty("visible", false);
-        }
-    }
-    if (m_osdObject) {
-        if (auto *rootObject = m_osdObject->rootObject()) {
             rootObject->setProperty("visible", false);
         }
     }

kded/osd.h
@@ -47,12 +47,9 @@ private Q_SLOTS:
 
 private:
     bool initOsd();
-    void showOsd();
-    void updatePosition();
 
     KScreen::OutputPtr m_output;
     QRect m_outputGeometry;
-    KDeclarative::QmlObject *m_osdObject = nullptr;
     KDeclarative::QmlObject *m_osdActionSelector = nullptr;
     QTimer *m_osdTimer = nullptr;
     int m_timeout = 0;

kded/osdmanager.cpp
@@ -67,68 +67,6 @@ OsdManager::~OsdManager()
 {
 }
 
-void OsdManager::showOutputIdentifiers()
-{
-    connect(new KScreen::GetConfigOperation(), &KScreen::GetConfigOperation::finished, this, &OsdManager::slotIdentifyOutputs);
-}
-
-void OsdManager::slotIdentifyOutputs(KScreen::ConfigOperation *op)
-{
-    if (op->hasError()) {
-        return;
-    }
-
-    const KScreen::ConfigPtr config = qobject_cast<KScreen::GetConfigOperation *>(op)->config();
-
-    const auto outputs = config->outputs();
-    for (const KScreen::OutputPtr &output : outputs) {
-        if (!output->isConnected() || !output->isEnabled() || !output->currentMode()) {
-            continue;
-        }
-        auto osd = m_osds.value(output->name());
-        if (!osd) {
-            osd = new KScreen::Osd(output, this);
-            m_osds.insert(output->name(), osd);
-        }
-
-        bool shouldShowSerialNumber = false;
-        if (output->edid()) {
-            shouldShowSerialNumber = std::any_of(outputs.cbegin(), outputs.cend(), [output](const auto &other) {
-                return other->id() != output->id() // avoid same output
-                    && other->edid() && other->edid()->name() == output->edid()->name() && other->edid()->vendor() == output->edid()->vendor();
-            });
-        }
-        osd->showOutputIdentifier(output, shouldShowSerialNumber);
-    }
-    m_cleanupTimer->start();
-}
-
-void OsdManager::showOsd(const QString &icon, const QString &text)
-{
-    hideOsd();
-
-    connect(new KScreen::GetConfigOperation(), &KScreen::GetConfigOperation::finished, this, [this, icon, text](KScreen::ConfigOperation *op) {
-        if (op->hasError()) {
-            return;
-        }
-
-        const KScreen::ConfigPtr config = qobject_cast<KScreen::GetConfigOperation *>(op)->config();
-
-        Q_FOREACH (const KScreen::OutputPtr &output, config->outputs()) {
-            if (!output->isConnected() || !output->isEnabled() || !output->currentMode()) {
-                continue;
-            }
-            auto osd = m_osds.value(output->name());
-            if (!osd) {
-                osd = new KScreen::Osd(output, this);
-                m_osds.insert(output->name(), osd);
-            }
-            osd->showGenericOsd(icon, text);
-        }
-        m_cleanupTimer->start();
-    });
-}
-
 OsdAction *OsdManager::showActionSelector()
 {
     hideOsd();

kded/osdmanager.h
@@ -30,8 +30,6 @@ public:
     ~OsdManager() override;
 
 public Q_SLOTS:
-    void showOutputIdentifiers();
-    void showOsd(const QString &icon, const QString &text);
     void hideOsd();
     KScreen::OsdAction *showActionSelector();
 

kded/qml/Osd.qml
@@ -1,53 +1,0 @@
-/*
-    SPDX-FileCopyrightText: 2014 Martin Klapetek <mklapetek@kde.org>
-    SPDX-FileCopyrightText: 2016 Sebastian Kügler <sebas@kde.org>
-
-    SPDX-License-Identifier: GPL-2.0-or-later
-*/
-
-import QtQuick 2.0
-import QtQuick.Window 2.2
-import org.kde.plasma.core 2.0 as PlasmaCore
-
-PlasmaCore.Dialog {
-    id: root
-    location: PlasmaCore.Types.Floating
-    type: PlasmaCore.Dialog.OnScreenDisplay
-    outputOnly: true
-
-    // OSD Timeout in msecs - how long it will stay on the screen
-    property int timeout: 5000
-
-    // Icon name to display
-    property string icon
-    property string infoText
-    property string outputName
-    property string modeName
-    property bool animateOpacity: false
-    property string itemSource
-    property QtObject osdItem
-
-    Behavior on opacity {
-        SequentialAnimation {
-            // prevent press and hold from flickering
-            PauseAnimation { duration: root.timeout * 0.8 }
-
-            NumberAnimation {
-                duration: root.timeout * 0.2
-                easing.type: Easing.InQuad
-            }
-        }
-        enabled: root.timeout > 0 && root.animateOpacity
-    }
-
-    mainItem: Loader {
-        source: itemSource
-        onItemChanged: {
-            if (item != undefined) {
-                item.rootItem = root;
-                root.osdItem = item
-            }
-        }
-
-    }
-}

kded/qml/OsdItem.qml
@@ -1,32 +1,0 @@
-/*
-    SPDX-FileCopyrightText: 2014 Martin Klapetek <mklapetek@kde.org>
-    SPDX-FileCopyrightText: 2016 Sebastian Kügler <sebas@kde.org>
-
-    SPDX-License-Identifier: GPL-2.0-or-later
-*/
-
-import QtQuick 2.5
-import QtQuick.Window 2.2
-import QtQuick.Layouts 1.10
-
-import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.extras 2.0 as PlasmaExtras
-
-ColumnLayout {
-    PlasmaCore.IconItem {
-        source: rootItem.icon
-
-        Layout.preferredHeight: PlasmaCore.Units.gridUnit * 10
-        Layout.preferredWidth: implicitHeight
-    }
-
-    PlasmaExtras.Heading {
-        text: rootItem.infoText
-
-        horizontalAlignment: Text.AlignHCenter
-        wrapMode: Text.WordWrap
-        maximumLineCount: 2
-
-        Layout.fillWidth: true
-    }
-}

kded/qml/OutputIdentifier.qml
@@ -1,46 +1,0 @@
-/*
-    SPDX-FileCopyrightText: 2016 Sebastian Kügler <sebas@kde.org>
-
-    SPDX-License-Identifier: GPL-2.0-or-later
-*/
-
-import QtQuick 2.5
-import QtQuick.Layouts 1.3
-import QtQuick.Window 2.2
-
-import org.kde.plasma.core 2.0 as PlasmaCore
-import org.kde.plasma.components 3.0 as PlasmaComponents
-
-ColumnLayout {
-
-    property QtObject rootItem
-
-    property string outputName: rootItem ? rootItem.outputName : ""
-    property string modeName: rootItem ? rootItem.modeName : ""
-
-    PlasmaComponents.Label {
-        id: displayName
-
-        Layout.maximumWidth: Screen.width * 0.8
-        Layout.maximumHeight: Screen.height * 0.8
-        Layout.margins: units.largeSpacing
-        Layout.bottomMargin: units.smallSpacing
-
-        text: root.outputName
-        font.pointSize: theme.defaultFont.pointSize * 3
-        horizontalAlignment: Text.AlignHCenter
-        wrapMode: Text.WordWrap
-        maximumLineCount: 2
-        elide: Text.ElideLeft
-    }
-
-    PlasmaComponents.Label {
-        id: modeLabel;
-
-        Layout.fillWidth: true
-        Layout.bottomMargin: units.largeSpacing
-
-        text: root.modeName;
-        horizontalAlignment: Text.AlignHCenter;
-    }
-}

tests/osd/main.cpp
@@ -14,36 +14,17 @@ int main(int argc, char **argv)
     QGuiApplication app(argc, argv);
 
     QCommandLineOption dbus = QCommandLineOption(QStringList() << QStringLiteral("d") << QStringLiteral("dbus"), QStringLiteral("Call over dbus"));
-    QCommandLineOption outputid =
-        QCommandLineOption(QStringList() << QStringLiteral("o") << QStringLiteral("outputidentifiers"), QStringLiteral("Show output identifier"));
-    QCommandLineOption icon = QCommandLineOption(QStringList() << QStringLiteral("i") << QStringLiteral("icon"),
-                                                 QStringLiteral("Icon to use for OSD"),
-                                                 QStringLiteral("preferences-desktop-display-randr"));
-    QCommandLineOption message = QCommandLineOption(QStringList() << QStringLiteral("m") << QStringLiteral("message"),
-                                                    QStringLiteral("Icon to use for OSD"),
-                                                    QStringLiteral("OSD Test"));
-    QCommandLineOption selector = QCommandLineOption({QStringLiteral("s"), QStringLiteral("selector")}, QStringLiteral("Show new screen action selector"));
     KScreen::OsdTest osdtest;
     QCommandLineParser parser;
     parser.addHelpOption();
     parser.addOption(dbus);
-    parser.addOption(outputid);
-    parser.addOption(icon);
-    parser.addOption(message);
-    parser.addOption(selector);
     parser.process(app);
 
     if (parser.isSet(dbus)) {
         osdtest.setUseDBus(true);
     }
-    if (parser.isSet(outputid)) {
-        osdtest.showOutputIdentifiers();
-    } else if (parser.isSet(selector)) {
-        osdtest.showActionSelector();
-    } else {
-        osdtest.showGenericOsd(parser.value(icon), parser.value(message));
-    }
-    if (parser.isSet(outputid)) { }
+
+    osdtest.showActionSelector();
 
     return app.exec();
 }

tests/osd/osdtest.cpp
@@ -35,40 +35,11 @@ OsdManager *OsdTest::getOsdManager()
     return m_osdManager;
 }
 
-void OsdTest::showOutputIdentifiers()
-{
-    if (!m_useDBus) {
-        QTimer::singleShot(5500, qApp, &QCoreApplication::quit);
-        getOsdManager()->showOutputIdentifiers();
-    } else {
-        QDBusMessage msg = QDBusMessage::createMethodCall(QLatin1String("org.kde.kscreen.osdService"),
-                                                          QLatin1String("/org/kde/kscreen/osdService"),
-                                                          QLatin1String("org.kde.kscreen.osdService"),
-                                                          QLatin1String("showOutputIdentifiers"));
-        // msg << icon << text;
-        QDBusConnection::sessionBus().asyncCall(msg);
-
-        qCWarning(KSCREEN_KDED) << "Sent dbus message.";
-        QTimer::singleShot(500, qApp, &QCoreApplication::quit);
-    }
-}
-
 void OsdTest::setUseDBus(bool yesno)
 {
     m_useDBus = yesno;
 }
 
-void OsdTest::showGenericOsd(const QString &icon, const QString &message)
-{
-    if (!m_useDBus) {
-        QTimer::singleShot(5500, qApp, &QCoreApplication::quit);
-        getOsdManager()->showOsd(!icon.isEmpty() ? icon : QStringLiteral("preferences-desktop-display-randr"),
-                                 !message.isEmpty() ? message : QStringLiteral("On-Screen-Display"));
-    } else {
-        qCWarning(KSCREEN_KDED) << "Implement me.";
-        QTimer::singleShot(100, qApp, &QCoreApplication::quit);
-    }
-}
 
 void OsdTest::showActionSelector()
 {
@@ -79,8 +50,14 @@ void OsdTest::showActionSelector()
             qApp->quit();
         });
     } else {
-        qCWarning(KSCREEN_KDED) << "Implement me.";
-        QTimer::singleShot(100, qApp, &QCoreApplication::quit);
+        QDBusMessage msg = QDBusMessage::createMethodCall(QLatin1String("org.kde.kscreen.osdService"),
+                                                          QLatin1String("/org/kde/kscreen/osdService"),
+                                                          QLatin1String("org.kde.kscreen.osdService"),
+                                                          QLatin1String("showActionSelector"));
+        QDBusConnection::sessionBus().asyncCall(msg);
+
+        qCWarning(KSCREEN_KDED) << "Sent dbus message.";
+        QTimer::singleShot(500, qApp, &QCoreApplication::quit);
     }
 }
 

tests/osd/osdtest.h
@@ -22,9 +22,6 @@ public:
     ~OsdTest() override;
 
     void setUseDBus(bool yesno);
-
-    void showGenericOsd(const QString &icon, const QString &message);
-    void showOutputIdentifiers();
     void showActionSelector();
 
 private: