8ac26551c8b58926649accc7c039e05274aac4f9
Nicolas Fella <nicolas.fella@gmx.de>
2024-04-17 13:19:12 +0200

    Use new plasma_add_applet API

--- plasmoid/kscreenapplet.cpp
+++ plasmoid/kscreenapplet.cpp
@@ -22,6 +22,8 @@
 
 #include <algorithm>
 
+K_PLUGIN_CLASS_WITH_JSON(KScreenApplet, "metadata.json")
+
 KScreenApplet::KScreenApplet(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
     : Plasma::Applet(parent, data, args)
 {
@@ -83,8 +85,6 @@ QVariant KScreenApplet::availableActions()
     // Need to wrap it in a QVariant, otherwise QML doesn't like the return type
     return QVariant::fromValue(KScreen::OsdAction::availableActions());
 }
-
-K_PLUGIN_CLASS(KScreenApplet)
 
 #include "kscreenapplet.moc"
 
