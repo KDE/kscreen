02ad1c7713ebb0e958a9c02c13a090f106957f0d
Nate Graham <nate@kde.org>
2025-06-10 13:57:58 -0600

    Genericize OSD action names
    Don't explicitly mention laptops or external screens since we can't
    assume the device's form factor. Instead consistently refer to
    screens as primary or secondary, re-using terminology we expose in the
    KCM.
    
    BUG: 445553
    FIXED-IN: 6.5.0

--- common/osdaction.cpp
+++ common/osdaction.cpp
@@ -25,11 +25,11 @@ using namespace KScreen;
 QList<OsdAction> OsdAction::availableActions()
 {
     return {
-        {SwitchToExternal, i18nd("kscreen_common", "Switch to external screen"), QStringLiteral("osd-shutd-laptop")},
-        {SwitchToInternal, i18nd("kscreen_common", "Switch to laptop screen"), QStringLiteral("osd-shutd-screen")},
-        {Clone, i18nd("kscreen_common", "Unify outputs"), QStringLiteral("osd-duplicate")},
-        {ExtendLeft, i18nd("kscreen_common", "Extend to left"), QStringLiteral("osd-sbs-left")},
-        {ExtendRight, i18nd("kscreen_common", "Extend to right"), QStringLiteral("osd-sbs-sright")},
+        {SwitchToExternal, i18nd("kscreen_common", "Only use secondary screen"), QStringLiteral("osd-shutd-laptop")},
+        {SwitchToInternal, i18nd("kscreen_common", "Only use primary screen"), QStringLiteral("osd-shutd-screen")},
+        {Clone, i18nd("kscreen_common", "Mirror screens"), QStringLiteral("osd-duplicate")},
+        {ExtendLeft, i18nd("kscreen_common", "Extend to left of primary screen"), QStringLiteral("osd-sbs-left")},
+        {ExtendRight, i18nd("kscreen_common", "Extend to right of primary screen"), QStringLiteral("osd-sbs-sright")},
         {NoAction, i18nd("kscreen_common", "Leave unchanged"), QStringLiteral("dialog-cancel")},
     };
 }
