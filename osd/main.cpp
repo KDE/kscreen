08349f16a88ac1afca445efc5c3102321a5bb5d9
Kai Uwe Broulik <kde@privat.broulik.de>
2025-12-18 15:38:12 +0100

    osd: Don't call useLayerShell
    It's not needed anymore and leaks into child processes.

--- osd/main.cpp
+++ osd/main.cpp
@@ -4,15 +4,12 @@
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
-#include <LayerShellQt/Shell>
-
 #include <QGuiApplication>
 
 #include "osdmanager.h"
 
 int main(int argc, char **argv)
 {
-    LayerShellQt::Shell::useLayerShell();
     QGuiApplication app(argc, argv);
     QGuiApplication::setQuitOnLastWindowClosed(false);
     KScreen::OsdManager osdManager;
